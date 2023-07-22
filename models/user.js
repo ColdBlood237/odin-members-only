213299c3c9515dd77f5607b1ea70ece0ad797368
Pixel <petngalan@gmail.com>
2023-07-22 11:52:40 +0200

    Initialize the project and create the models

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  username: { type: String, required: true, maxLength: 100 },
+  password: { type: String, required: true, maxLength: 100 },
+  membership_status: { type: String, required: true, maxLength: 100 },
+});
+
+UserSchema.virtual("full_name").get(function () {
+  let fullname = "";
+
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+
+  return fullname;
+});
+
+module.exports = mongoose.model("User", UserSchema);
